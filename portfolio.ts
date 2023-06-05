97c52fc5cb8f38dcf5ba57839676b89f1bf0a366
somboonnontaganok <somboonnontaganok@gmail.com>
2023-06-05 07:46:28 +0700

    adjust portfolio to new

--- portfolio.ts
+++ portfolio.ts
@@ -138,6 +138,28 @@ export const educationInfo: EducationType[] = [
       'I have synthesized ten tetrazole derivatives.',
     ],
   },
+  {
+    schoolName: 'Generation Thailand',
+    subHeader: 'Cirtificate of Junior Software Developer Cohort 4',
+    duration: 'Feb 2023 - May 2023',
+    desc: 'Binary Tree Project (Activity Tracking',
+    grade: '95.65%',
+    descBullets: [
+      'I work hard to study and practice every day. And I became a good Junior Software Developer.',
+      'I am now well versed in frontend and backend with the MERN Stack.',
+    ],
+  },
+  {
+    schoolName: 'The Master Academy',
+    subHeader: 'I am an AWS re/Start THBAN1 cloud engineer learner in Thailand.',
+    duration: 'May 2023 - August 2023',
+    desc: 'Learning from 18:00 - 22:00 and  will end at the end of August 2023',
+    grade: '97.67% cumulative score',
+    descBullets: [
+      'I work hard to study and practice every day. And I hope to be a good AWS Cloud Engineer.',
+
+    ],
+  }
 ];
 
 export const experience: ExperienceType[] = [
@@ -166,13 +188,13 @@ export const projects: ProjectType[] = [
     name: 'developer-portfolio',
     desc: 'Software Developer Portfolio Template built with react.js and next.js bootstrap that helps you showcase your work and skills as a software developer.',
     github: 'https://github.com/somboonnontaganok/poon-portfolio',
-    link: 'https://front-end-binary-b1.vercel.app/',
+    link: 'https://poon-portfolio.vercel.app/',
   },
   {
     name: 'Binary Tree Fit',
     desc: 'Full stack software developer with activity tracker .',
     github: 'https://github.com/somboonnontaganok/Front-end-binary-b1',
-    link: 'https://front-end-binary-b1.vercel.app/',
+    link: 'https://front-activity-beta.vercel.app/',
   },
 ];
 
@@ -188,7 +210,7 @@ export const seoData: SEODataType = {
     'A passionate Full Stack Web Developer and AWS Cloud Engineer.',
   author: 'Somboon Nontaganok',
   image: 'https://cdn.pixabay.com/photo/2017/07/25/01/22/cat-2536662_1280.jpg',
-  url: 'https://poon-portfolio.vercel.app/',
+  url: 'https://front-activity-beta.vercel.app/',
   keywords: [
     'Somboon',
     'Somboon Nontaganok',
